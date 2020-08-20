6601fa9b5c5b82a09708825eb0e2e65f3cbfc103
Revanth Raghu <revabob@gmail.com>
2020-08-20 15:38:20 +0530

    added files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# revanthraghu.github.io
+# Revanth's profile
+
+*** Work in progress ***
+
 A webpage showing my profile and the projects I have worked on.
---
+++ precache-manifest.ac1afa6daf68a4363057db9730c9ada3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf459fc8e6b29a500187f8655f2ba9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e71afd78730855782eb6",
+    "url": "./static/js/2.4c33cdea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4c33cdea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68858b3b443c52f9a54",
+    "url": "./static/js/main.8ea28819.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);

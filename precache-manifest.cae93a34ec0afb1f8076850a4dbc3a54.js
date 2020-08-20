afd4ec2ac38e4de193c57ee3bbd8f9948a5e3b0c
Revanth Raghu <revabob@gmail.com>
2020-08-20 15:46:03 +0530

    Updates

---
+++ precache-manifest.cae93a34ec0afb1f8076850a4dbc3a54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2446ee9cc09f8c1847bfc10318b10774",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e71afd78730855782eb6",
+    "url": "./static/js/2.4c33cdea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4c33cdea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba89c25dbf3699cc904c",
+    "url": "./static/js/main.00252454.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
